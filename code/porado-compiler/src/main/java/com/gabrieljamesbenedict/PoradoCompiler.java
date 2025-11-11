b0f5391b5b1c161e1808d88f802b86a79b46554d
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-12 03:08:24 +0800

    feat: add Launch4J to pom.xml

--- code/porado-compiler/src/main/java/com/gabrieljamesbenedict/PoradoCompiler.java
+++ code/porado-compiler/src/main/java/com/gabrieljamesbenedict/PoradoCompiler.java
@@ -1,13 +1,8 @@
 package com.gabrieljamesbenedict;
 
-/**
- * Hello world!
- *
- */
 public class PoradoCompiler 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
