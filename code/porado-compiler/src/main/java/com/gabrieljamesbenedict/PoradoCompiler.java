34b7bf2aa8e8f49edf70006f7c300fa8c4c215e1
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-12 02:48:35 +0800

    refactor: rename default App.java to PoradoCompiler.java

---
+++ code/porado-compiler/src/main/java/com/gabrieljamesbenedict/PoradoCompiler.java
@@ -0,0 +1,14 @@
+package com.gabrieljamesbenedict;
+
+/**
+ * Hello world!
+ *
+ */
+public class PoradoCompiler 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ code/porado-compiler/src/test/java/com/gabrieljamesbenedict/PoradoCompilerTest.java
@@ -0,0 +1,19 @@
+package com.gabrieljamesbenedict;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PoradoCompilerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
