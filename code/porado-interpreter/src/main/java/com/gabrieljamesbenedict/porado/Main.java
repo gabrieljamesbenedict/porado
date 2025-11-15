fe4c2407c7d40b6f0bcdd7603b09dd2c9d03b052
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-15 18:50:44 +0800

    faet: add Main class

---
+++ code/porado-interpreter/src/main/java/com/gabrieljamesbenedict/porado/Main.java
@@ -0,0 +1,11 @@
+package com.gabrieljamesbenedict.porado;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PoradoInterpreter porado = new PoradoInterpreter(args);
+
+    }
+
+}
