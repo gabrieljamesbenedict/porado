1e7131dc91bc81164c69627641f43e10f05fad7e
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-26 04:49:07 +0800

    feat: add ASTInterpreter

---
+++ code/porado-interpreter/src/main/java/com/gabrieljamesbenedict/porado/ASTInterpreter.java
@@ -0,0 +1,14 @@
+package com.gabrieljamesbenedict.porado;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ASTInterpreter {
+
+    private final AbstractSyntaxTree ast;
+
+    public void interpret() {
+
+    }
+
+}
